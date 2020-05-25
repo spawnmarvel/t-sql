f393a0bd78ca174cd79bed86a98da52e9970f56a
Jan Espen Kleivane <espenkleivane@gmail.com>
2020-05-25 19:37:52 +0200

    lesson 1

---
+++ module1.sql
@@ -0,0 +1,8 @@
+-- Module 1
+-- T-sql is a set based language, .sql, GO keyword separate batches (Stored procedures, break into batches)
+-- GO
+-- Do stuff, insert, update
+-- GO, treat it as a separate batches, just like separate files
+
+
+
