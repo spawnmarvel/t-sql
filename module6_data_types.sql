95b3fb7a6ef20bef70b05f8c7078efcb9a7e0b8b
spawnmarvel <espenkleivane@gmail.com>
2020-06-22 20:55:51 +0200

    numeric data types

--- module6_data_types.sql
+++ module6_data_types.sql
@@ -1,2 +1,16 @@
 -- Lesson 1
 -- Introducing SQL Server Data Types
+
+-- Numeric Data Types
+-- Data type, Range, Storage
+-- tinyint, 0-255, 1
+-- smallint, -32,768, 32,768, 2
+-- int, -2,147,486,648, 2,147,486,648, 4
+-- big int, +/- 9 quintillion, 8
+-- bit, 1, 0 or NULL, 1
+
+
+
+-- Why not use bigint for all tinyints , due to storage.
+-- If status for product, 5 values, 1,2, 3, 4, 5 then use tinyint
+-- If values exceed the change, must think about storage and types
