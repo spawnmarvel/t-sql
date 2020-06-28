173ad26b6c384f1c1cec69bfe8c8516a25e3d02f
spawnmarvel <espenkleivane@gmail.com>
2020-06-28 20:19:40 +0200

    char

--- module6_data_types.sql
+++ module6_data_types.sql
@@ -31,3 +31,79 @@ SELECT CAST(1234 AS BINARY(4)) as result
 
 --
 
+-- Other data types
+-- xml, The advantage of the xml data type over storing XML in a character data type is that the xml data type allows XML nodes and attributes to be queried within a T-SQL query using XQuery expressions
+-- uniqueidentifier, GUID, be generated within SQL Server by using the NEWID() 
+SELECT NEWID() as guid_from_new_id
+1DD193F3-F303-4FFB-9616-34344E555E96
+
+-- hierarchyid
+-- rowversion
+-- geometry
+-- geography
+-- sql_variant
+-- table
+
+
+-- Data Type Precedence
+-- Determines whicg data type will be chosen when expression of different types are combined
+-- Operations have to be with like data types
+-- Convert = CAST
+-- When combining or comparing two expressions with different data types, the one lower on this list will be converted to the type that is higher
+-- Partial list ranked according to precedence
+-- xml, datetime2, date, time, decimal, int, tinyint, nvarchar, char
+
+DECLARE @my_tiny_int AS TINYINT =25
+DECLARE @my_int AS INT = 9999;
+SELECT @my_tiny_int;
+SELECT @my_int;
+SELECT @my_tiny_int + @my_int;
+
+-- When are Data Types Converted?
+
+-- Data type cenversion scenarios
+-- When data is moved, compared to or combined with other data
+-- During assignment of variable
+
+-- Implicit conversion
+-- When comparing data of one data type to another
+-- Transparent to the user
+WHERE <column of smallint type> = <value of int type>
+
+-- Explicit conversion
+-- Use CAST or CONVERT functions
+
+CAST(unitprice AS INT)
+--
+-- Implicit conversion integer data types
+DECLARE @my_tiny_int AS TINYINT =25
+DECLARE @my_int AS INT = 9999;
+SELECT @my_tiny_int + @my_int;
+-- Result
+10024
+-- Implicit conversion integer and char
+DECLARE @my_char AS char(5)='6';
+DECLARE @my_int2 AS int = 1;
+SELECT @my_char + @my_int2;
+-- Result
+7
+-- SQL Server will automatically attempt to perform an implicit conversion from a lower-precedence data type to a higher-precedence data type. 
+-- Failing
+DECLARE @my_char AS char(5) = 'six';
+DECLARE @my_int AS INT = 1;
+SELECT @my_char + @my_int;
+
+Msg 245, Level 16, State 1, Line 4
+Conversion failed when converting the varchar value 'six  ' to data type int.
+
+Completion time: 2020-06-28T20:14:46.6610969+02:00
+
+-- Character Data Types
+-- SQL Server support two kinds of character data as fixed-width of variable-width data
+-- ASCI, 1 byte pr character system
+-- UNICODE, 2 bute pr character system
+-- Single byte:
+-- Char, varchar
+
+
+
