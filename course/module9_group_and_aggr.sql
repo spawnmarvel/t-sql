cc04368a6fd8b3b2ba4e0a19f061d5496bf73e8b
spawnmarvel <espenkleivane@gmail.com>
2020-07-13 16:24:46 +0200

    LESSON 2 GROUP BY

--- course/module9_group_and_aggr.sql
+++ course/module9_group_and_aggr.sql
@@ -63,5 +63,34 @@ SUM(COALESCE(c2,0)) AS SUM_NON_NULLS,
 COUNT(*) AS COUNT_ALL_ROWS,
 COUNT(c2) AS COUNT_NON_NULLS,
 AVG(c2) AS [AVG]
- FROM [test].[dbo].[t1]
- 
+FROM [test].[dbo].[t1]
+
+-- Using the GROUP BY Clause
+-- Group aggregation for values
+-- if only aggregate functions, then just one row, if group by then unique rows with meaning
+
+-- 
+SELECT SalesPersonID, COUNT(*) AS TOTAL_SALES
+FROM [AdventureWorks].[Sales].[SalesOrderHeader]
+GROUP BY SalesPersonID
+
+-- Order of operation
+
+-- 4 SELECT SalesPersonID, COUNT(*) AS TOTAL_SALES
+-- 1 FROM [AdventureWorks].[Sales].[SalesOrderHeader]
+-- 2 WHERE YEAR(OrderDate)= 2005
+-- 3 GROUP BY SalesPersonID
+
+-- FAIL WITH SUBTOTAL
+SELECT SubTotal,  SalesPersonID, COUNT(*) AS TOTAL_SALES
+FROM [AdventureWorks].[Sales].[SalesOrderHeader]
+WHERE YEAR(OrderDate)= 2005
+GROUP BY SalesPersonID
+-- Msg 8120, Level 16, State 1, Line 2
+-- Column 'AdventureWorks.Sales.SalesOrderHeader.SubTotal' is invalid in the select list because it is not contained in either an aggregate function or the GROUP BY clause.
+
+SELECT COUNT(*) AS TOTAL_SALES
+FROM [AdventureWorks].[Sales].[SalesOrderHeader]
+WHERE YEAR(OrderDate)= 2005
+GROUP BY SalesPersonID
+
