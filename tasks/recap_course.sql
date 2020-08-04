c2ab6a4e939887c532781182981ea4967d2cc52a
spawnmarvel <espenkleivane@gmail.com>
2020-08-04 12:02:23 +0200

    CERT

--- tasks/recap_course.sql
+++ tasks/recap_course.sql
@@ -1,7 +1,7 @@
 -- More learning from the course
 -- https://docs.microsoft.com/en-us/previous-versions/sql/sql-server-2008-r2/ms365303%28v%3dsql.105%29
 
--- Agregate functions
+-- Built in functions
 -- https://docs.microsoft.com/en-us/previous-versions/sql/sql-server-2008-r2/ms173454(v=sql.105)
 
 --Choose db
@@ -9,11 +9,14 @@ use AdventureWorks;
 GO
 -- get columns
 EXEC sp_columns SalesOrderDetail;
+
 --aggregate functions
-SELECT AVG(UnitPrice)
+SELECT AVG(UnitPrice) as res_avg, MIN(UnitPrice) res_min, MAX(UnitPrice) res_max
 FROM Sales.SalesOrderDetail;
 
-SELECT MIN(UnitPrice)
-FROM Sales.SalesOrderDetail
 
+-- more built in
 
+-- GROUP BY
+
+--
